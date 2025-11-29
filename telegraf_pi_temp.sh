c9d9dacda6feb7bcf4ef491a8415cd06219600b3
sikow32 <sikow@outlook.de>
2025-11-29 19:08:01 +0100

    Update telegraf_pi_temp.sh

--- telegraf_pi_temp.sh
+++ telegraf_pi_temp.sh
@@ -5,3 +5,4 @@ gpu=$(/opt/vc/bin/vcgencmd measure_temp)
 gpu_temp=${gpu#$prefix}
 gpu_temp=${gpu_temp%$suffix}
 echo -e "{\"cpu\":"$(</sys/class/thermal/thermal_zone0/temp)", \"gpu\":$gpu_temp}"
+#cccc
